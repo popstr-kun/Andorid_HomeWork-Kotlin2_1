76228996afe7502f420276fc746f553f1d9260d1
popstr-kun <hw38752920@gmail.com>
2022-11-01 23:51:20 +0800

    ver1.0

---
+++ app/src/main/java/com/example/andorid_homework_kotlin2_1/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.andorid_homework_kotlin2_1
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.Fragment
+import com.example.andorid_homework_kotlin2_1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var activityResultLauncher: ActivityResultLauncher<Intent>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //setContentView(R.layout.activity_main)
+
+        binding.btnChoice.setOnClickListener{
+            val intent = Intent(this,MainActivity2::class.java)
+//          startActivityForResult(intent,1)
+            activityResultLauncher.launch(intent)
+        }
+
+        activityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            result ->
+            if(result.resultCode == 101 && result.data != null){
+                val b = result.data?.extras?.let {
+                    binding.tvMeal.text = "飲料: ${it.getString("drink")}\n\n甜度: ${it.getString("sugar")}\n\n冰塊: ${it.getString("ice")}\n\n"
+                }
+            }
+        }
+
+
+
+
+//        fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//            super.onActivityResult(requestCode, resultCode, data)
+//            println("hello ")
+//            if(requestCode == 1 && resultCode == Activity.RESULT_OK){
+//                println("fdfsf")
+//            }
+//        }
+
+    }
+}
---
+++ app/src/main/java/com/example/andorid_homework_kotlin2_1/MainActivity2.kt
@@ -0,0 +1,67 @@
+package com.example.andorid_homework_kotlin2_1
+
+import android.app.Activity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.example.andorid_homework_kotlin2_1.databinding.ActivityMain2Binding
+import com.example.andorid_homework_kotlin2_1.databinding.ActivityMainBinding
+
+class MainActivity2 : AppCompatActivity() {
+    private lateinit var binding: ActivityMain2Binding
+    private var sugar = "無糖"
+    private var iceOpt = "去冰"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_main2)
+
+        binding.radioGroup.setOnCheckedChangeListener(){
+            _,
+            i -> when(i){
+                binding.radioButton1.id -> sugar = "無糖"
+                binding.radioButton2.id -> sugar = "少糖"
+                binding.radioButton3.id -> sugar = "半糖"
+                binding.radioButton4.id -> sugar = "全糖"
+            }
+        }
+
+        binding.radioGroup2.setOnCheckedChangeListener(){
+            _,
+            i-> when(i){
+                binding.radioButton5.id -> iceOpt = "去冰"
+                binding.radioButton6.id -> iceOpt = "微冰"
+                binding.radioButton7.id -> iceOpt = "少冰"
+                binding.radioButton8.id -> iceOpt = "正常冰"
+            }
+        }
+
+        binding.btnSend.setOnClickListener{
+
+            val drink = binding.edDrink.text.toString()
+            val intent = Intent()
+            val bundle = Bundle()
+
+            with(bundle){
+                putString("sugar",sugar)
+                putString("drink",drink)
+                putString("ice",iceOpt)
+            }
+
+
+            intent.putExtras(bundle)
+
+            setResult(101,intent)
+            finish()
+
+        }
+    }
+
+
+}
+
+
